74cfd06e87a559d4395eb883f89ee2df9d1e5e94
yosaidi <younes.saidi.23@ump.ac.ma>
2025-02-24 14:44:54 +0000

    validation of input

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,1 +1,18 @@
-import validationModule  from './validinput.js';
+import { validPuzzle, validWords }  from './validinput.js';
+
+const emptyPuzzle = `2001
+0..0
+1000
+0..0`
+const words = ['casa', 'alan', 'ciao', 'anta']
+
+
+const crosswordSolver = (puzzle, words) => {
+    if (!validPuzzle(puzzle) || !validWords(words)) {
+        return "Error"
+    };
+    const grid = emptyPuzzle.split('\n').map(row => row.split(''));
+    return grid
+};
+
+console.log(crosswordSolver(emptyPuzzle,words))
--- validinput.js
+++ validinput.js
@@ -1,21 +1,20 @@
-let conditions ={
+let conditions = {
     array: Array.isArray,
     lowercase: (str) => /^[a-z]+$/.test(str),
-    puzzlepattern : (str) => /^[0-9.\n]+$/.test(str) ,
+    puzzlepattern: (str) => /^[0-9.\n]+$/.test(str),
     string: (str) => typeof str === "string",
 };
 
-const validPuzzle = (puzzle) => conditions.puzzlepattern(puzzle);
+export const validPuzzle = (puzzle) => conditions.puzzlepattern(puzzle);
 
-const validWords = (arr) => {
-      if (!conditions.array(arr)) {
+export const validWords = (arr) => {
+    if (!conditions.array(arr)) {
         return false;
     }
 
-    return arr.every(word => 
+    return arr.every(word =>
         conditions.string(word) && conditions.lowercase(word)
     );
 };
 
 
-export default { validPuzzle, validWords, conditions };

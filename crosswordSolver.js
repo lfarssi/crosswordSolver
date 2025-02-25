a62626cfe62cdb0c8c853e32fe2dbc5f5e71bdf9
yosaidi <younes.saidi.23@ump.ac.ma>
2025-02-25 13:40:45 +0000

    crossword

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,10 +1,29 @@
 import { validPuzzle, validWords } from './validinput.js';
 
-const emptyPuzzle = `2001
-0..0
-1000
-0..0`
-const words = ['casa', 'alan', 'ciao', 'anta']
+
+const puzzle = `..1.1..1...
+10000..1000
+..0.0..0...
+..1000000..
+..0.0..0...
+1000..10000
+..0.1..0...
+....0..0...
+..100000...
+....0..0...
+....0......`
+const words = [
+  'popcorn',
+  'fruit',
+  'flour',
+  'chicken',
+  'eggs',
+  'vegetables',
+  'pasta',
+  'pork',
+  'steak',
+  'cheese',
+]
 
 
 const crosswordSolver = (puzzle, words) => {
@@ -12,16 +31,14 @@ const crosswordSolver = (puzzle, words) => {
         return "Error"
     };
     const grid = puzzle.split('\n').map(row => row.split(''));
+    // grid.forEach(row => console.log(row.join(' ')));
 
     const wordDirections = [];
-    console.log(('Puzzle:\n', grid))
+
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[row].length; col++) {
             const current = grid[row][col]
             if (current !== '.' && current !== '\n') {
-                // console.log('current place:', current);
-                // console.log('row: ', grid[row]);
-                // console.log('column:', grid[row][col]);
 
                 if (col === 0 || grid[row][col - 1] === '.' || grid[row][col - 1] === '\n') {
                     let wordLength = 0;
@@ -30,7 +47,7 @@ const crosswordSolver = (puzzle, words) => {
                         wordLength++;
                     }
 
-                    wordDirections.push({ direction: 'horizontal', row, col, length: wordLength })
+                    if (wordLength > 1) wordDirections.push({ direction: 'horizontal', row, col, length: wordLength });
                 };
 
                 if (row === 0 || grid[row - 1][col] === '.' || grid[row - 1][col] === '\n') {
@@ -40,13 +57,51 @@ const crosswordSolver = (puzzle, words) => {
                         wordLength++;
                     };
 
-                    wordDirections.push({ direction: 'vertical', row, col, length: wordLength })
+                    if (wordLength > 1) wordDirections.push({ direction: 'vertical', row, col, length: wordLength });
 
                 }
             };
         };
     };
-    return wordDirections;
-};
 
-console.log(crosswordSolver(emptyPuzzle, words))
+    // console.log("words length: ", words.length, "\nNumber of slots: ", wordDirections.length)
+    words.sort((a, b) => a.length - b.length)
+    wordDirections.sort((a, b) => a.length - b.length)
+    // console.log("\nSorted Words:", words);
+    // console.log("\nSorted Slots:", wordDirections);
+
+    const matchedSlots = [];
+    const usedWords = new Set();
+
+    for (const slot of wordDirections) {
+        for (const word of words) {
+            if (word.length === slot.length && !usedWords.has(word)){
+                matchedSlots.push({ word, ...slot });
+                usedWords.add(word);
+                break;
+            };
+        };
+
+    };
+
+    const rows = grid[0].length;
+    const cols = grid.length;
+
+    let result = Array.from({length: rows}, ()=>Array(cols).fill('.'));
+
+    matchedSlots.forEach( ({word,direction,row,col}) => {
+        for (let  i = 0 ; i< word.length; i++) {
+             if (direction === 'horizontal'){
+                result [row][col+i] = word[i];
+             } else if ( direction === 'vertical') {
+                result [row + i] [col] = word [i];
+             }
+        }
+    });
+
+    result = result.map(row => row.join('')).join('\n');
+    return result;
+    ;
+}
+
+console.log(crosswordSolver(puzzle, words))
--- validinput.js
+++ validinput.js
@@ -1,20 +1,19 @@
 let conditions = {
     array: Array.isArray,
-    lowercase: (str) => /^[a-z]+$/.test(str),
-    puzzlepattern: (str) => /^[0-9.\n]+$/.test(str),
+    isAword: (word) => word.length >= 2,
+    puzzlepattern: (str) => /^[0-2.\n]+$/.test(str),
     string: (str) => typeof str === "string",
+    unique : (arr) => new Set(arr).size === arr.length,
 };
 
 export const validPuzzle = (puzzle) => conditions.puzzlepattern(puzzle);
 
 export const validWords = (arr) => {
-    if (!conditions.array(arr)) {
+    if (!conditions.array(arr) || !conditions.unique(arr)) {
         return false;
     }
 
     return arr.every(word =>
-        conditions.string(word) && conditions.lowercase(word)
+        conditions.string(word) && conditions.isAword(word)
     );
 };
-
-
